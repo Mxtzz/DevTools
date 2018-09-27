f637ef4a919bbc1a48d395718676dd6d3ef17752
Mxtzz <mazw2016@hotmail.com>
2018-09-27 20:34:25 +0800

    add

--- index.js
+++ index.js
@@ -6,9 +6,24 @@ window.onload = function () {
         var colorDecC = document.getElementById('colorDecC').value;
         var theHex = (Number(colorDecA).toString(16) + Number(colorDecB).toString(16) + Number(colorDecC).toString(16)).toUpperCase();
         console.log(theHex)
-        document.getElementById('theHex').innerText = theHex;
-        bool = document.execCommand('copy');//还不能用
-        console.log(bool)
+        document.getElementById('theHex').innerText = "#"+theHex;
+        // bool = document.execCommand('copy');//还不能用
+        // console.log(bool)
+    }
+
+    transToHexBtn = () => {
+        var transToDec = document.getElementById('colorHex').value;
+        var theDec = transToDec.toString().split("");
+        let theDecA = parseInt((theDec[0]+theDec[1]),16);
+        let theDecB = parseInt((theDec[2]+theDec[3]),16);
+        let theDecC = parseInt((theDec[4]+theDec[5]),16);
+        console.log(theDec)
+        console.log(theDecA)
+        document.getElementById('theDecA').innerText = theDecA;
+        document.getElementById('theDecB').innerText = theDecB;
+        document.getElementById('theDecC').innerText = theDecC;
+        // bool = document.execCommand('copy');//还不能用
+        // console.log(bool)
     }
 
 }
